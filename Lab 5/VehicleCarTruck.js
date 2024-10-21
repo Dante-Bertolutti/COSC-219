a3f4923aae399e5b0d9949016f50b8886c068315
Dante-Bertolutti <dantebertolutti@outlook.com>
2024-10-21 12:59:02 -0700

    Midday2

--- Lab 5/VehicleCarTruck.js
+++ Lab 5/VehicleCarTruck.js
@@ -10,4 +10,40 @@ class Vehicle {
     getVehicleInfo() {
         return `${this.model}, ${this.make}, ${this.year}, ${this.mileage} km, Cost: $${this.price}`;
     }
+
+    drive(km){
+        this.mileage += km;
+    }
+}
+class Car extends Vehicle {
+    constructor(make, model, year, mileage, price, fuelType) {
+        super(make, model, year, mileage, price);
+        this.fuelType = fuelType; 
+    }
+    getCarInfo() {
+        return `${this.make} ${this.model} (${this.year}), ${this.mileage} KM, Cost: $${this.price}, Fuel: ${this.fuelType}`;
+    }
+    applyDiscount(discount) {
+            this.price -= this.price * discount;
+    }
+}
+class Truck extends Vehicle {
+    constructor(make, model, year, mileage, price, cargoCapacity) {
+        super(make, model, year, mileage, price);
+        this.cargoCapacity = cargoCapacity;
+        this.currentCargo = 0;
+    }
+
+    getTruckInfo() {
+        return `${this.make} ${this.model} (${this.year}), ${this.mileage} KM, Cost: $${this.price}, Cargo: ${this.currentCargo} kilograms`;
+    }
+
+    loadCargo(kg) {
+        if (this.currentCargo + kg <= this.cargoCapacity) {
+            this.currentCargo += kg;
+            console.log(`${kg} loaded`);
+        } else {
+            console.log(`Max Capacity`);
+        }
+    }
 }
