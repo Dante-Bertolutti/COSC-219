7703fa896fc189e65cb36d9d9d7aec0fdd642d36
Dante-Bertolutti <dantebertolutti@outlook.com>
2024-10-21 13:19:00 -0700

    LibrarySystemDone

--- Lab 5/LibrarySystem.js
+++ Lab 5/LibrarySystem.js
@@ -38,3 +38,31 @@ const createLibrary = (books) => {
         }
     }
 }
+function testlibrary(){
+    const book1 = createBook("Harry Potter and the Goblet of Fire", "J.K. Rowling", 2000, 10.99);
+    const book2 = createBook("Harry Potter and the Order of the Phoenix", "J.K. Rowling", 2003, 25.00);
+    const book3 = createBook("A Dance with Dragons", "George R. R. Martin", 2011, 9.99);
+    
+    const myLibrary = createLibrary();
+    myLibrary.addBook(book1);
+    myLibrary.addBook(book2);
+    myLibrary.addBook(book3);
+    
+    const totalValue = myLibrary.getTotalValue();
+    console.log(`Total value of all books: $${totalValue.toFixed(2)}`);
+    
+    const searchedBook = myLibrary.findBook("Harry Potter and the Goblet of Fire");
+    console.log(searchedBook.getBookInfo());
+    myLibrary.removeBook("Harry Potter and the Order of the Phoenix");
+
+    const newTotalValue = myLibrary.getTotalValue();
+    console.log(`New total value after removing a book: $${newTotalValue.toFixed(2)}`);
+    
+    book1.setDiscount(0.40);
+    
+    const totalValueAfterDiscount = myLibrary.getTotalValue();
+    console.log(`Total value after applying discount: $${totalValueAfterDiscount.toFixed(2)}`);
+
+}
+
+testlibrary();
