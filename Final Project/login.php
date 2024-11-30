ab1c38e3cb53d2428c18200700612b62c251236a
Dante-Bertolutti <dantebertolutti@outlook.com>
2024-11-29 16:57:00 -0800

    Final Sync

--- Final Project/login.php
+++ Final Project/login.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $database = "u256863105_ocPlayerLogin";
-$username = "u256863105_dantebertolutt";
+$username = "u256863105_dantebertolutti";
 $password = "dBert7777";
  
 // Create connection
